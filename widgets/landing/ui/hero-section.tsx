693902d2eec8367f6565e9a9a072d0921dd07606
leey00nsu <dbstndla1212@naver.com>
2025-05-13 21:05:24 +0900

    feat: 랜딩 페이지 커버 추가

--- widgets/landing/ui/hero-section.tsx
+++ widgets/landing/ui/hero-section.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 import { Button } from "@/shared/ui/button";
 import { ArrowRight } from "lucide-react";
+import { Cover } from "./cover";
 
 export function HeroSection() {
   return (
     <section className="mb-16">
-      <h1 className="text-4xl md:text-5xl font-bold mb-4">
-        Leemage: 당신의 이미지를 위한 클라우드 솔루션
+      <h1 className="relative z-20 mx-auto mt-6 max-w-7xl bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 bg-clip-text py-6 text-center text-4xl font-semibold text-transparent dark:from-neutral-800 dark:via-white dark:to-white md:text-4xl lg:text-6xl">
+        Upload Everything <br /> on <Cover>space</Cover>
       </h1>
-      <p className="text-lg text-muted-foreground mb-8 max-w-2xl mx-auto">
-        Oracle Cloud Infrastructure 기반의 강력한 이미지 관리 및 제공 서비스를
-        경험하세요. 프로젝트별로 이미지를 손쉽게 관리하고 필요한 형태로 변환하여
-        사용하세요.
-      </p>
       <Link href="/projects">
         <Button size="lg">
           지금 시작하기 <ArrowRight className="ml-2 h-5 w-5" />
