14af29d06c824bea95e716e7b4a8d5e5abeb4588
leey00nsu <dbstndla1212@naver.com>
2025-06-06 20:34:59 +0900

    refactor: 필요없는 페이지 삭제

--- app/[locale]/playground/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UploadProgress } from "@/entities/images/upload/ui/upload-progress";
-
-export default function Playground() {
-  return (
-    <div>
-      <UploadProgress isUploading={true} error={new Error("test")} />
-    </div>
-  );
-}
